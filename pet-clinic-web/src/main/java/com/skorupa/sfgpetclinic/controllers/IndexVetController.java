05ff3481481eb2f688a893cbf3221720b3f88c52
noobProgrammer1 <jaceksko91@gmail.com>
2019-05-05 17:34:24 +0200

    created vet index page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/com/skorupa/sfgpetclinic/controllers/IndexVetController.java
@@ -0,0 +1,14 @@
+package com.skorupa.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexVetController {
+
+    @RequestMapping({"vets/index","vets","vets/index.html"})
+    public String listVets(){
+
+        return "/vets/index";
+    }
+}
