0803405fded81a2f6f2d4525be3e44d10c458648
noobProgrammer1 <jaceksko91@gmail.com>
2019-05-13 21:14:48 +0200

    added all vets list closes #14

--- pet-clinic-web/src/main/java/com/skorupa/sfgpetclinic/controllers/IndexVetController.java
+++ pet-clinic-web/src/main/java/com/skorupa/sfgpetclinic/controllers/IndexVetController.java
@@ -1,13 +1,23 @@
 package com.skorupa.sfgpetclinic.controllers;
 
+import com.skorupa.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexVetController {
 
+    private final VetService vetService;
+
+    public IndexVetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets/index","vets","vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
         return "/vets/index";
     }
 }
