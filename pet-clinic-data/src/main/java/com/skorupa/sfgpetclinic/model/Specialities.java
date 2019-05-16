b2028fa78aad6749108f56e86e5f8606def1e9e1
noobProgrammer1 <jaceksko91@gmail.com>
2019-05-16 21:06:35 +0200

    added Specialities and added to Vet closes #25

---
+++ pet-clinic-data/src/main/java/com/skorupa/sfgpetclinic/model/Specialities.java
@@ -0,0 +1,14 @@
+package com.skorupa.sfgpetclinic.model;
+
+public class Specialities extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/skorupa/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/skorupa/sfgpetclinic/model/Vet.java
@@ -1,4 +1,12 @@
 package com.skorupa.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialities> specialities;
+
+
+
+
 }
